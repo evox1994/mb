f7508024832391f5c47d7996ec52b493bb87186e
evox1994 <dmitrybatukhtin@gmail.com>
2018-12-30 15:22:02 +0700

    ывамвамв

--- js/index.js
+++ js/index.js
@@ -16,13 +16,13 @@ $(document).ready(function(){
 		var el = $(this).data('f');
 		$('.b-3 .filter li').removeClass('active');
 		$(this).addClass('active');
-		$('.b-3-slider').removeClass('active');
+		/*$('.b-3-slider').removeClass('active');
 		setTimeout(function(){
 			$('.b-3-slider').css('display','none');
 			$(el).css('display','block');
 			$(el).addClass('active');
 			$('.b-3-slider').slick('reinit');
-		},300);
+		},300);*/
 	});
 
 	$('.b-3-slider').slick({
